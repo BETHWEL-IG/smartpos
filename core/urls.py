a27cbec55ddcc56b89f992e88abea6ff9f942d58
bethwel <bethwelifedha@gmail.com>
2024-11-25 20:32:58 +0300

    Add product crud

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path("", views.home, name='home'),
     path('add-product/', views.add_product, name='add_product'),
     path('products/', views.product_list, name='product_list'),
+    path('products/update/<int:pk>/', views.update_product, name='update_product'),
+    path('products/delete/<int:pk>/', views.delete_product, name='delete_product'),
+
+
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from . forms import ProductForm
 from . models import Product
 
@@ -17,6 +17,28 @@ def add_product(request):
         form = ProductForm()
     return render(request, 'core/add_product.html', {'form': form})
 
+# listing products
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'core/product_list.html', {'products': products})
+
+# update functinality
+def update_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'core/add_product.html', {'form': form})
+
+
+# delete functionality
+def delete_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'core/confirm_delete.html', {'product': product})
